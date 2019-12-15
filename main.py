5ce12d1db70e88720323dc1ea903f7277647e59c
Igor Kovorotniy <kovorotniy@gmail.com>
2019-12-15 18:23:54 +0300

    Added predestrian detection

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
+""" Pedestrian detection """
 import cv2
-import numpy as np
+# import numpy as np
 
 cap = cv2.VideoCapture('static-cctv.mp4')
-
+#cap = cv2.VideoCapture('dust-pedestrians.mp4')
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
 
 while cap.isOpened():
+    # calculate diff on two frames
     diff = cv2.absdiff(frame1, frame2)
+    # convert to shades of gray
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
     
     _,thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
 
     dilated = cv2.dilate(thresh, None, iterations=3)
+    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-    contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+        if cv2.contourArea(contour) < 700:
+            continue
+        cv2.rectangle(frame1, (x, y), (x+w, y+h), (255, 144, 30),2)
 
-    cv2.drawContours(frame1, contours, -1, (255, 144, 30), 2)
+        cv2.putText(frame1, "State: {}".format('Movement'), 
+        (10, 20), cv2.FONT_HERSHEY_SIMPLEX,1, (0, 0, 0),2)
+    # draw all movement objects contours 
+    #cv2.drawContours(frame1, contours, -1, (0,0,0), 2)
     
-    cv2.imshow('inter', frame1)
+    cv2.imshow('Pedestrians', frame1)
     frame1 = frame2
 
     ret, frame2 = cap.read()
